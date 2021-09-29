c6715833e73fa362e3143c0350d28c75f8cac220
iamanupam1 <anupamdhakal20@gmail.com>
2021-09-29 16:28:36 +0545

    Second V

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,3 +95,19 @@ itemHealth.forEach((el) => {
         next = next.nextElementSibling
     }
 })
+
+
+$('.vertical-slider .vertical .item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    for (var i=1;i<2;i++) {
+    next=next.next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    }
+    });
